436914fc18c98b107d5d14bfe4c75d0a06f7a3a4
Nicholas <nwittstruck@users.noreply.github.com>
2022-08-11 22:18:55 +0200

    [Chore] Mix.config is deprecated #59 (#70)
    * [Chore] Mix.config is deprecated #59

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :qrstorage,
   ecto_repos: [
@@ -60,4 +60,4 @@ config :dart_sass,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
