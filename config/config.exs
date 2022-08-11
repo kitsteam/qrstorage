ae9c351430ab0b1e799ae7bf19914c31ebb63d99
Nicholas <nicholas@b310.de>
2022-08-11 22:12:19 +0200

    [Chore] Mix.config is deprecated #59

--- config/config.exs
+++ config/config.exs
@@ -60,4 +60,4 @@ config :dart_sass,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
