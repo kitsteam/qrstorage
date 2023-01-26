fb12e07f540f4d2b013ff091dd765c5f72f1fbea
Nicholas <nwittstruck@users.noreply.github.com>
2023-01-26 21:37:22 +0100

    [Bug] Warning that amplitudejs should be marked as external #130 (#131)

--- config/config.exs
+++ config/config.exs
@@ -44,9 +44,10 @@ config :phoenix, :json_library, Jason
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.14.1",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args:
+      ~w(js/app.js --bundle --target=es2016 --external:amplitudejs --external:*.webmanifest --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
