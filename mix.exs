71ca4e029dddb952ac2c4ebb2eb7aad2a61dbbee
Nicholas <nwittstruck@users.noreply.github.com>
2024-06-13 11:54:45 +0200

    Chore upgrade postgrex 0.18.0 (#272)
    * build(deps): bump postgrex from 0.17.5 to 0.18.0
    
    Bumps [postgrex](https://github.com/elixir-ecto/postgrex) from 0.17.5 to 0.18.0.
    - [Release notes](https://github.com/elixir-ecto/postgrex/releases)
    - [Changelog](https://github.com/elixir-ecto/postgrex/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/elixir-ecto/postgrex/compare/v0.17.5...v0.18.0)
    
    ---
    updated-dependencies:
    - dependency-name: postgrex
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: remove ssl_opts to conform with new postgrex 0.18.0 syntax
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Qrstorage.MixProject do
       {:phoenix, "1.7.12"},
       {:phoenix_ecto, "4.6.1"},
       {:ecto_sql, "3.11.2"},
-      {:postgrex, "0.17.5"},
+      {:postgrex, "0.18.0"},
       {:phoenix_html, "4.1.1"},
       {:phoenix_html_helpers, "1.0.1"},
       {:phoenix_view, "2.0.3"},
