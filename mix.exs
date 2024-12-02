df8c5ad318369d07e73bb30fccc816875737238c
nwittstruck <nwittstruck@users.noreply.github.com>
2024-12-02 10:58:27 +0100

    chore: set same_site to strict

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Qrstorage.MixProject do
   def project do
     [
       app: :qrstorage,
-      version: "0.4.6",
-      elixir: "~> 1.11",
+      version: "0.5.0",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
